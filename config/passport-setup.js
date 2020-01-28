6492a388eb7e3f02cdd571dde2eb47ce36cef35f
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2020-01-28 12:39:39 -0700

    created modal for login

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -10,8 +10,16 @@ passport.use(
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
 }, (accessToken, refreshToken, profile, done) => {
-//passport callback function
-console.log( profile);
+//check to see if user already exists in our db 13-14
+// User.findOne({googleId: profile.id}).then((currentUser) => {
+// if(currentUser){
+//     //already have user
+//     console.log("user is: ", currentUser);
+// } else {
+//     //not a current user, create user
+// }
+// })
+
 var User = {
     username: profile.displayName,
     googleId: profile.id
--- server.js
+++ server.js
@@ -48,3 +48,4 @@ db.sequelize.sync(syncOptions).then(function() {
 });
 
 module.exports = app;
+
