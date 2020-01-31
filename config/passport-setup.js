184dafae9429c29b04ca209152887f6361fcf5ca
Richard Hamp <54290959+RichardHamp@users.noreply.github.com>
2020-01-31 12:59:22 -0700

    authentication and page navigation (#29)

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,38 +1,43 @@
-var passport = require("passport");
-var GoogleStrategy = require("passport-google-oauth20");
-var keys = require("./keys");
-var User = require("../models/user");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./keys");
+const db = require("../models");
+
+passport.serializeUser(function(user,done) {
+  console.log("user serialised");
+  done(null, user.id);
+  });
+
+passport.deserializeUser(function (id,done) {
+  console.log("user de-serialised");
+  User.findById(id, function(err,user) {
+    done(err, user);
+  });
+ });
 
 passport.use(
-  new GoogleStrategy(
-    {
-      //options for the strat
-      callbackURL: "/auth/google/redirect",
-      clientID: keys.google.clientID,
-      clientSecret: keys.google.clientSecret
-    },
-    function(accessToken, refreshToken, profile, done) {
-      //check to see if user already exists in our db 13-14
-      // User.findOne({googleId: profile.id}).then((currentUser) => {
-      // if(currentUser){
-      //     //already have user
-      //     console.log("user is: ", currentUser);
-      // } else {
-      //     //not a current user, create user
-      // }
-      // })
-
-      var User = {
-        username: profile.displayName,
-        googleId: profile.id
-      };
-      console.log("New User:");
-      console.log(User);
-      // did thid to fix prettier problems - consider removing this optional parameter
-      done.end();
-      // .save().then((newUser) =>{
-      //     console.log("new user created: " + newUser)
-      // })
-    }
-  )
+    new GoogleStrategy({
+        // options for google strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
+        callbackURL: '/auth/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        // check if user already exists in our own db
+        db.User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                // already have this user
+                console.log('user is: ', currentUser);
+                done(null, currentUser);
+             } else {
+                          //if not, create user in our db
+                          db.User.create({
+                            name: profile.displayName,
+                            googleId: profile.id
+                          }).then(function (newUser) {
+                            console.log("new user created: " + newUser.name);
+                            done(null, newUser);
+                          });
+            }
+        });
+    })
 );
