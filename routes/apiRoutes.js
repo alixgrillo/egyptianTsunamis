ca8a53833bbc32000334c625b3b7857870cf186f
Alix Grillo <alix.grillo2+github@gmail.com>
2020-02-01 16:50:04 -0700

    deleted unnecessary code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,6 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  //Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
   /// NEED TO UPDATE USER ID TO COOKIE
   // Making sure this merged correctly
   app.post("/api/charitySave", function(req, res) {
@@ -36,13 +23,4 @@ module.exports = function(app) {
       res.json(dbCategory);
     });
   });
-
-  // // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(
-  //     dbExample
-  //   ) {
-  //     res.json(dbExample);
-  //   });
-  // });
 };
