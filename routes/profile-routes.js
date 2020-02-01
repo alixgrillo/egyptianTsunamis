f50ac42cd548693b41ba6e1bcdf88b848ffdc41c
Alix Grillo <alix.grillo2+github@gmail.com>
2020-01-31 19:46:00 -0700

    fixing formatting to adhere to eslint standards

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,18 +1,18 @@
-const router = require('express').Router();
+var router = require("express").Router();
 
-const authCheck = (req, res, next) => {
-    if(!req.session.passport){
-        res.redirect('/auth/login');
-        console.log("not a user")
-    } else {
-        console.log("USER!!!")
-        next();
-    }
+var authCheck = function(req, res, next) {
+  if (!req.session.passport) {
+    res.redirect("/auth/login");
+    console.log("not a user");
+  } else {
+    console.log("USER!!!");
+    next();
+  }
 };
 
-router.get('/', authCheck, (req, res) => {
-    res.render("profile", {user: req.session.passport})
-    // + req.user.username);
+router.get("/", authCheck, function(req, res) {
+  res.render("profile", { user: req.session.passport });
+  // + req.user.username);
 });
 
 module.exports = router;
